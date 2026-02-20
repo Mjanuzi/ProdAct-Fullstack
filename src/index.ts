d66215fd9edee7bd47915073042ce5a3de085d14
Mergim Januzi <mergim.januzi@gmail.com>
2026-02-20 14:18:48 +0100

    categories route set and works with test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { prisma } from "./lib/prisma";
 
 const app = express();
 const port = process.env.PORT ?? 3001;
+
+app.use(cors());
+app.use(express.json());
 
 //Healthcheck
 app.get("/health", async (_req, res) => {
@@ -12,6 +16,19 @@ app.get("/health", async (_req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ ok: false });
+  }
+});
+
+//List all Categories
+app.get("/api/categories", async (_req, res) => {
+  try {
+    const categories = await prisma.category.findMany({
+      orderBy: { name: "asc" },
+    });
+    res.json(categories);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
