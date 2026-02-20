325418ce686194081a93e4f25680e4ad9d0002a0
Mergim Januzi <mergim.januzi@gmail.com>
2026-02-20 14:27:11 +0100

    get one category route set and works with tests

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { prisma } from "./lib/prisma";
+import { error } from "node:console";
 
 const app = express();
 const port = process.env.PORT ?? 3001;
@@ -26,6 +27,29 @@ app.get("/api/categories", async (_req, res) => {
       orderBy: { name: "asc" },
     });
     res.json(categories);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+//get ONE category
+app.get("/api/categories/:id", async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    if (Number.isNaN(id)) {
+      res.status(400).json({ error: "Invalid id" });
+      return;
+    }
+    const category = await prisma.category.findUnique({
+      where: { id },
+      include: { products: true },
+    });
+    if (!category) {
+      res.status(404).json({ error: "Not Found" });
+      return;
+    }
+    res.json(category);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal server error" });
